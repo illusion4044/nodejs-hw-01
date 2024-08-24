2dc1916c48bb47de47600bffa459eba6a5717bde
Andrii <117983189+illusion4044@users.noreply.github.com>
2024-08-24 20:54:26 +0300

    changes

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,11 +1,12 @@
-import * as fs from "node:fs";
+import *  as fs from 'node:fs/promises';
 
 import { PATH_DB_CONTACTS } from '../constans/contacts.js';
 
 import { createFakeContact } from '../utils/createFakeContact.js';
 
 const generateContacts = async (number) => {
-    const contactList = JSON.parse(await fs.readFile(PATH_DB_CONTACTS, "utf-8"));
+    const data = await fs.readFile(PATH_DB_CONTACTS, 'utf-8');
+    const contactList = JSON.parse(data);
     const newContactList = Array(number).fill(0).map(createFakeContact)
     // await fs.writeFile(PATH_DB_CONTACTS, JSON.stringify(newContactList));
     contactList.push(...newContactList);
